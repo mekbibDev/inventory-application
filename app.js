24a9c8cec8e6e7a0fd2c5fa63386172e6b24b8d7
mekbibDev <mekbibtare@gmail.com>
2024-01-08 18:28:49 +0300

    Add necessary middlewares

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
+require("dotenv").config();
 const createError = require("http-errors");
 const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
-
+const compression = require("compression");
+const helmet = require("helmet");
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
 
@@ -13,6 +15,11 @@ const app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 
+if (process.env.NODE_ENV === "development") {
+  app.use(compression());
+}
+
+app.use(helmet());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
