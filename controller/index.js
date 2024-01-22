b2ecd2d3a0eb013c1338bd9bc312b8b2d8a0b792
mekbibDev <mekbibtare@gmail.com>
2024-01-22 14:11:05 +0300

    Validate category before filtering gadgets

--- controller/index.js
+++ controller/index.js
@@ -1,6 +1,7 @@
 const Gadget = require("../model/gadget");
 const Categories = require("../model/category");
 const Category = require("../model/category");
+const { query, matchedData, validationResult } = require("express-validator");
 
 async function getAll(req, res, next) {
   try {
@@ -11,20 +12,30 @@ async function getAll(req, res, next) {
     next(error);
   }
 }
-async function filter(req, res, next) {
-  try {
-    const category = await Category.findById(req.query["category"])
-      .populate("gadgets")
-      .setOptions({ sanitizeFilter: true });
-    const categories = await Categories.find({});
-    res.render("index", {
-      title: "Gadget Inventory",
-      gadgets: category.gadgets,
-      selectedCategory: category,
-      categories,
-    });
-  } catch (error) {
-    next(error);
-  }
-}
+const filter = [
+  query("category").notEmpty().escape(),
+  async function (req, res, next) {
+    try {
+      const result = validationResult(req);
+      if (result.isEmpty()) {
+        const data = matchedData(req);
+        const category = await Category.findById(data.category).populate(
+          "gadgets",
+        );
+        const categories = await Categories.find({});
+        res.render("index", {
+          title: "Gadget Inventory",
+          gadgets: category.gadgets,
+          selectedCategory: category,
+          categories,
+        });
+      } else {
+        throw new Error("A valid category must be chosen");
+      }
+    } catch (error) {
+      next(error);
+    }
+  },
+];
+
 module.exports = { getAll, filter };
